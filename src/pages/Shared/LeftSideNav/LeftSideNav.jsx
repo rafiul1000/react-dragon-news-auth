524619ba0b59c36f15156ff327bc52066b439fe2
rafiul1000 <rafiulislam5075@gmail.com>
2023-10-03 13:29:14 +0600

    Left side navigation

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const LeftSideNav = () => {
+
+    const [categories, setCategories] = useState([]);
+
+
+    useEffect(() => {
+        fetch('categories.json')
+        .then(res => res.json())
+        .then(data => setCategories(data))
+    }, [])
+
     return (
         <div>
-            <h2 className="text-2xl">Left Side</h2>
+            <h2 className="text-[#403F3F] text-xl font-semibold">All Categories</h2>
+            {
+                categories.map(category => <Link 
+                    className="block text-[#9F9F9F] text-xl font-medium ml-4 mt-8" 
+                    key={category.id} 
+                    to={`/category/${category.id}`}>{category.name}</Link>)
+            }
         </div>
     );
 };
